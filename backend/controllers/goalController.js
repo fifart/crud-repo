88a6b0d6a070e3f1f990773ac56dbe08c9d8ec7d
TASSOS P <fifart@gmail.com>
2022-12-06 08:30:04 +0200

    repo initialized

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,25 +1,47 @@
 const asyncHandler = require('express-async-handler')
 
-const getGoals = async (req,res) => {
-    res.status(200).json({message:'Get goals'})
-}
+const Goal = require('../models/goalModel')
+
+const getGoals = asyncHandler(async (req,res) => {
+
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
+    
+})
 
 const setGoal = asyncHandler(async (req,res) => {
     if(!req.body.text) {
         res.status(400)
-        throw new Error('Please add a txt field')
+        throw new Error('Please add a text field')
     }
 
+    const goal = await Goal.create({
+        text: req.body.text
+    })
 
-    res.status(200).json({message:'Set goal'})
+    res.status(200).json(goal)
 })
 
 const updateGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`Update goal ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('This goal doesn\'t exist')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true, })
+    
+    res.status(200).json(updatedGoal)
 })
 
 const deleteGoal = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`Delete goal ${req.params.id}`})
+
+    const deletedGoal = await Goal.findByIdAndRemove(req.params.id)
+
+    res.status(200).json({message:`Deleted goal with id ${deletedGoal}`})
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
 const port = process.env.PORT || 6000
 
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
